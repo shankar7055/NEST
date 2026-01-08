c54afd7616572188ccad3f82cc89cdbeb6cc8dbd
shankar7055 <shankarpratap220@gmail.com>
2026-01-08 21:34:43 +0530

    Remove Orchid integration and cleanup

--- commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git config user.email "orchids@ai.com"
-git config user.name "Orchids AI"
-rm -rf tmp_nest
-git add .
-git commit -m "Initial commit from Orchids"
-git push origin master --dry-run
